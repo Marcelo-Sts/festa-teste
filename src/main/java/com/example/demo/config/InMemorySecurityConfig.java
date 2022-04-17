cc004686cbe146b28d63695595382297760ef33f
Marcelo-Sts <stsmarcelo11@gmail.com>
2022-04-17 00:55:08 -0300

    Segunda Atualização

--- src/main/java/com/example/demo/config/InMemorySecurityConfig.java
+++ src/main/java/com/example/demo/config/InMemorySecurityConfig.java
@@ -1,12 +1,10 @@
 package com.example.demo.config;
 
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication
 .builders.AuthenticationManagerBuilder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 public class InMemorySecurityConfig {
